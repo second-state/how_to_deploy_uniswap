80180f1091ab937ff6bddbfdcb4ee5ccce4df6fd
tpmccallum <mistermac2008@gmail.com>
2020-09-08 12:47:46 +1000

    Print counter value

--- uniswap_interface/scrape_hex_digests.py
+++ uniswap_interface/scrape_hex_digests.py
@@ -14,23 +14,26 @@ for individual_dir in dirs_to_process:
     for (root, dirs, files) in os.walk(individual_dir):
         for name in files:
             file_path = os.path.join(root, name)
-            print("Processing: " + file_path)
-            try:
-                fr = open(file_path)
-                fd = fr.read()
-                print("Reading: " + file_path)
-                temp_inner_object = {}
-                hash_40_results = hash_40.findall(fd)
-                hash_64_results = hash_64.findall(fd)
-                if len(hash_40_results) > 0 or len(hash_64_results) > 0:
-                    counter = counter + len(hash_40_results)
-                    counter = counter + len(hash_64_results)
-                    temp_inner_object["hash_40"] = hash_40_results
-                    temp_inner_object["hash_64"] = hash_64_results
-                    temp_output[file_path] = temp_inner_object
-            except:
-                print("Skipping: " + file_path)
-                fr.close()
+            if file_path.endswith(".r") or file_path.endswith(".py"):
+                print("Skipping " + file_path)
+            else:
+                print("Processing: " + file_path)
+                try:
+                    fr = open(file_path)
+                    fd = fr.read()
+                    print("Reading: " + file_path)
+                    temp_inner_object = {}
+                    hash_40_results = hash_40.findall(fd)
+                    hash_64_results = hash_64.findall(fd)
+                    if len(hash_40_results) > 0 or len(hash_64_results) > 0:
+                        counter = counter + len(hash_40_results)
+                        counter = counter + len(hash_64_results)
+                        temp_inner_object["hash_40"] = hash_40_results
+                        temp_inner_object["hash_64"] = hash_64_results
+                        temp_output[file_path] = temp_inner_object
+                except:
+                    print("Not valid UTF8, skipping: " + file_path)
+                    fr.close()
 json_formatted_str = json.dumps(temp_output, indent=2)
 print("Results\n" + json_formatted_str)
 print("Writing these results to hex_values.json file ...")
