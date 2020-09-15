332ab2c49866464e189fb5494d83992100b45532
tpmccallum <mistermac2008@gmail.com>
2020-09-15 15:07:17 +1000

    Facilitate query to contract variables

--- uniswap_v2/query_uniswap_contracts.js
+++ uniswap_v2/query_uniswap_contracts.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Web3 = require("web3");
-const URL = 'https://mainnet.infura.io/v3/0920fdea265848a9b49dd72674c088a7';
+//const URL = 'https://mainnet.infura.io/v3/0920fdea265848a9b49dd72674c088a7';
+const URL = 'http://oasis-ssvm-amber.secondstate.io:8545';
 const web3 = new Web3(new Web3.providers.HttpProvider(URL));
 
 function get_data(_message) {
@@ -19,10 +20,15 @@ function get_data(_message) {
     var data = await get_data();
     var data_object = JSON.parse(data);
 
+// uniswap_factory
+    var uniswapV1Contract = new web3.eth.Contract(data_object.abi.uniswap_factory, data_object.contract_address.uniswap_factory);
+    var feeTo = uniswapV1Contract.methods.exchangeTemplate().call()
+        feeTo.then(function(resultexchangeTemplate) {
+        console.log("V1 exchangeTemplate is set to: " + resultexchangeTemplate);
+    })
+
 // Factory
-    var uniswapV2Abi = data_object.abi.uniswap_v2;
-    var uniswapFactoryContract = new web3.eth.Contract(uniswapV2Abi, "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
-    console.log("Checking feeTo ...");
+    var uniswapFactoryContract = new web3.eth.Contract(data_object.abi.uniswap_v2, data_object.contract_address.uniswap_v2);
     var feeTo = uniswapFactoryContract.methods.feeTo().call()
         feeTo.then(function(resultFeeTo) {
         console.log("V2 Factory feeTo is currently set to: " + resultFeeTo);
@@ -31,30 +37,55 @@ function get_data(_message) {
         feeToSetter.then(function(resultFeeToSetter) {
         console.log("v2 Factory feeToSetter is currently set to: " + resultFeeToSetter);
     })
+    var allPairsLength = uniswapFactoryContract.methods.allPairsLength().call()
+        allPairsLength.then(function(resultallPairsLength) {
+        console.log("v2 Factory allPairsLength is currently set to: " + resultallPairsLength);
+    })
+
+// Router
+    var uniswapRouterContract = new web3.eth.Contract(data_object.abi.router, data_object.contract_address.router);
+    var factory = uniswapRouterContract.methods.factory().call()
+        factory.then(function(resultfactory) {
+        console.log("V2 Router02 factory is currently set to: " + resultfactory);
+    })
+    var factory = uniswapRouterContract.methods.WETH().call()
+        factory.then(function(resultWETH) {
+        console.log("V2 Router02 WETH is currently set to: " + resultWETH);
+    })
 
 // WETH
-    var uniswapWETHAbi = data_object.abi.weth;
-    var uniswapWethContract = new web3.eth.Contract(uniswapWETHAbi, "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
-    console.log("Checking feeTo ...");
+    var uniswapWethContract = new web3.eth.Contract(data_object.abi.weth, data_object.contract_address.weth);
     var feeTo = uniswapWethContract.methods.totalSupply().call()
         feeTo.then(function(resultTotalSupply) {
         console.log("WETH totalSupply is set to: " + resultTotalSupply);
     })
-/*
-// These (factoryV1 and router) are not public vars they are declared as immutable vars so we were unable to access them
+
+// Multicall
+    var uniswapMilticallContract = new web3.eth.Contract(data_object.abi.multicall, data_object.contract_address.multicall);
+    var getLastBlockHash = uniswapMilticallContract.methods.getLastBlockHash().call()
+        getLastBlockHash.then(function(resultgetLastBlockHash) {
+        console.log("Multicall getLastBlockHash is set to: " + resultgetLastBlockHash);
+    })
+    var getCurrentBlockTimestamp = uniswapMilticallContract.methods.getCurrentBlockTimestamp().call()
+        getCurrentBlockTimestamp.then(function(resultgetCurrentBlockTimestamp) {
+        console.log("Multicall getCurrentBlockTimestamp is set to: " + resultgetCurrentBlockTimestamp);
+    })
+    var getCurrentBlockDifficulty = uniswapMilticallContract.methods.getCurrentBlockDifficulty().call()
+        getCurrentBlockDifficulty.then(function(resultgetCurrentBlockDifficulty) {
+        console.log("Multicall getCurrentBlockDifficulty is set to: " + resultgetCurrentBlockDifficulty);
+    })
+    var getCurrentBlockGasLimit = uniswapMilticallContract.methods.getCurrentBlockGasLimit().call()
+        getCurrentBlockGasLimit.then(function(resultgetCurrentBlockGasLimit) {
+        console.log("Multicall getCurrentBlockGasLimit is set to: " + resultgetCurrentBlockGasLimit);
+    })
+    var getCurrentBlockCoinbase = uniswapMilticallContract.methods.getCurrentBlockCoinbase().call()
+        getCurrentBlockCoinbase.then(function(resultgetCurrentBlockCoinbase) {
+        console.log("Multicall getCurrentBlockCoinbase is set to: " + resultgetCurrentBlockCoinbase);
+    })
+
 // Migrator
-    var uniswapMigratorAbi = data_object.abi.migrator;
-    var uniswapMigratorContract = new web3.eth.Contract(uniswapMigratorAbi, "0x16D4F26C15f3658ec65B1126ff27DD3dF2a2996b");
-    console.log("Checking feeTo ...");
-    var factoryV1 = uniswapMigratorContract.methods.factoryV1().call()
-        factoryV1.then(function(resultFactoryV1) {
-        console.log("Migrator's factory V1 is set to: " + resultFactoryV1);
-    })
-    var routerVar = uniswapMigratorContract.methods.router().call()
-        routerVar.then(function(resultRouter) {
-        console.log("Migrator's router is set to: " + resultRouter);
-    })
-*/
+    var uniswapMigratorContract = new web3.eth.Contract(data_object.abi.migrator, data_object.contract_address.migrator);
+    console.log("Migrator: " + JSON.stringify(uniswapMigratorContract.methods));
 
 // end await
 })();
