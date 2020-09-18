c9f64e3fbbc6b8997e69820aa1f22aff50a7973c
tpmccallum <mistermac2008@gmail.com>
2020-09-18 14:20:56 +1000

    Clean up .bak files for chain and rpc modification

--- uniswap_interface/change_chain_id.py
+++ uniswap_interface/change_chain_id.py
@@ -9,9 +9,12 @@ dirs_to_process = ['../../src/', '../../build/', '../../node_modules/@uniswap/']
 for individual_dir in dirs_to_process:
     for (root, dirs, files) in os.walk(individual_dir):
         for name in files:
-            if name.endswith("rr"):
+            print("Processing: " + os.path.join(root, name))
+            subprocess.call(['sed', '-ir', sed_command_v2_12, os.path.join(root, name)])
+# Clean up old files
+for individual_dir in dirs_to_process:
+    for (root, dirs, files) in os.walk(individual_dir):
+        for name in files:
+            if name.endswith("r"):
                 print("Cleaning up old files")
                 os.remove(os.path.join(root, name))
-            else:
-                print("Processing: " + os.path.join(root, name))
-                subprocess.call(['sed', '-ir', sed_command_v2_12, os.path.join(root, name)])
--- uniswap_interface/change_rpc.py
+++ uniswap_interface/change_rpc.py
@@ -1,4 +1,5 @@
 import os
+import json
 import subprocess
 
 f = open("../installation_data.json", "r")
@@ -16,7 +17,7 @@ for individual_dir in dirs_to_process:
         for name in files:
             print("Processing: " + os.path.join(root, name))
             subprocess.call(['sed', '-ir', sed_command_i_o, os.path.join(root, name)])
-
+# Clean up old files
 for individual_dir in dirs_to_process:
     for (root, dirs, files) in os.walk(individual_dir):
         for name in files:
